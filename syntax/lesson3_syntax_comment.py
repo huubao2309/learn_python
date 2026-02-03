161cdc5e0a0f982fc8ae0eff6c176670e4a64094
Huu Bao Nguyen <huubao2309@gmail.com>
2026-02-03 21:06:15 +0700

    Lesson 3: Syntax Comment

---
+++ syntax/lesson3_syntax_comment.py
@@ -0,0 +1,19 @@
+# Lesson 3: https://www.youtube.com/watch?v=XeURcCz160o&list=PLux-_phi0Rz0Ngc01o_GYe0JUGjL1yIAm&index=3
+# Comment in Python
+
+## Note in Python:
+### No ";" end line
+### indent in python is important => point a new block code
+
+# Has 2 ways comment in Python:
+
+# 1. Use # for commenting a line
+## This is a notice code
+
+# 2. Use """ """ for commenting multi line
+## Use '' or "" in Python is the same
+"""
+This is multi line
+You need to notice it
+"""
+print('This is multi line of program')
